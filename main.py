e859ac90283d429e89dcc3c65990f5bd8304b1b0
Alex <ahanna0608@gmail.com>
2025-10-01 16:42:45 -0400

    Add help

--- main.py
+++ main.py
@@ -2,17 +2,26 @@ from scripts.task import Task
 from scripts.sort import Sort
 from scripts.search import Search
 import datetime
+import sys
+
+if (("-h" or "--help") in sys.argv):
+    print("Usage:\n python3 ./main.py [options]\n")
+    print("Options:")
+    print(" -a, --add\tadd a task")
+    print("\n -h, --help\tdisplay this help")
+elif (("-a" or "--add") in sys.argv):
+    name = input("Name: ")
+    description = input("Description: ")
+    priority = input("Priority (1-5): ")
+    due_date = input("Due Date (MM/DD/YY 00:00 AM): ")
 
 tasks = []
 
 with open("data_set.csv") as f:
     for line in f.read().splitlines()[1:]: # removes first row of file
-        #print(Task.from_csv(line))
         tasks.append(Task.from_csv(line))
 
 tasks = Sort.heap(tasks, Sort.By.NAME)
 
-tasks = Search.linear(tasks, "name")
-
-for task in tasks:
-    print(task)
+# for task in tasks:
+#     print(task)
--- scripts/task.py
+++ scripts/task.py
@@ -1,19 +1,19 @@
-import datetime
+from datetime import datetime
 from scripts.sort import Sort
 
 class Task:
-    def __init__(self, id, name, description, completed, priority, dueDate, creationDate):
+    def __init__(self, id, name, description, completed, priority, due_date, creation_date=datetime.now()):
         self.id = id
         self.name = name
         self.description = description
         self.completed = completed
         self.priority = priority
-        self.dueDate = dueDate
-        self.creationDate = creationDate
+        self.due_date = due_date
+        self.creation_date = creation_date
 
     def __str__(self):
         s = f"""{"▗▄▖" if self.completed else "┌─┐"} \x1B[1m{self.name}\x1B[0m: {self.description}
-{"▝▀▘" if self.completed else "└─┘"} \x1B[2m{self.dueDate.strftime("%#m/%#d/%y, %#I:%M %p")}\x1B[0m"""
+{"▝▀▘" if self.completed else "└─┘"} \x1B[2m{self.due_date.strftime("%#m/%#d/%y, %#I:%M %p")}\x1B[0m"""
         return s
 
     def contains(self, query):
@@ -23,17 +23,17 @@ class Task:
     def from_csv(cls, line):
         values = line.split(",")
         # helpful datetime formatting cheat sheet: https://strftime.org/
-        due_date = datetime.datetime.strptime(values[5], "%m/%d/%y %I:%M %p")
-        creation_date = datetime.datetime.strptime(values[6], "%m/%d/%y %I:%M %p")
+        due_date = datetime.strptime(values[5], "%m/%d/%y %I:%M %p")
+        creation_date = datetime.strptime(values[6], "%m/%d/%y %I:%M %p")
         return cls(values[0], values[1], values[2], "True" == values[3], values[4], due_date, creation_date)
     
     def compare(self, task, by=Sort.By.NAME):
         if by == Sort.By.NAME:
             return self.name < task.name
         elif by == Sort.By.DUE_DATE:
-            return self.dueDate < task.dueDate
+            return self.due_date < task.due_date
         elif by == Sort.By.CREATION_DATE:
-            return self.creationDate < task.creationDate
+            return self.creation_date < task.creation_date
         elif by == Sort.By.COMPLETED:
             return self.completed > task.completed
         elif by == Sort.By.PRIORITY:
