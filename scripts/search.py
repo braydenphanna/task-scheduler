0e8652cd3c1db028915c97da0cd7b788eb82be2e
Brayden Hanna <Bhanna0608@gmail.com>
2025-10-02 17:07:43 -0400

    add binary search

--- scripts/search.py
+++ scripts/search.py
@@ -2,9 +2,18 @@ from scripts.task import Task
 
 class Search:
     # returns single task
+    # only works for IDs 
     def binary(tasks, query):
-        pass
-
+        mid = int(len(tasks)/2)
+        if len(tasks) == 1 and not tasks[mid].contains(query):
+            return 
+        elif tasks[mid].id == query: 
+            return tasks[mid]
+        elif tasks[mid].id < query:
+            return Search.binary(tasks[mid:],query)
+        elif tasks[mid].id >query:
+            return Search.binary(tasks[:mid],query)
+  
     # returns multiple tasks
     def linear(tasks, query):
         results = []
