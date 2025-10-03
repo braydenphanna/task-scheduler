2722f541ea54a2e7241bffbc6e6856bbbd95ba54
alexphanna <ahanna0608@gmail.com>
2025-10-02 23:15:53 -0400

    Add color substring

--- ui/search.py
+++ ui/search.py
@@ -36,8 +36,8 @@ class Search:
             if(task.contains(Search.query)): 
                 temp = task.clone()
 
-                temp.description = Search.__replaceIgnorecase(temp.description, Search.query)
-                temp.name = Search.__replaceIgnorecase(temp.name, Search.query)
+                temp.description = Search.__colorSubstring(temp.description, Search.query)
+                temp.name = Search.__colorSubstring(temp.name, Search.query)
 
                 print(temp)
 
@@ -45,19 +45,66 @@ class Search:
         # ANSI escape code: clear screen and move cursor to top-left
         print("\033[2J\033[H", end="")
 
-    def __replaceIgnorecase(string, query):
-        if len(query)<=0: return string
+    def __colorSubstring(string, substring):
+        # 1. get indexes of all occurrences of substring in string
+        #    goes backwards for step 2
+        occurrences = []
+        for i in range(len(string) - len(substring) + 1, -1, -1):
+            if (string[i:i + len(substring)].lower() == substring.lower()):
+                occurrences.append(i)
+
+        # 2. add parentheses around occurrence
+        for occurrence in occurrences:
+            string = string[:occurrence] + "(" + string[occurrence:]
+            string = string[:occurrence + len(substring) + 1] + ")" + string[occurrence + len(substring) + 1:]
         
-        lowerString = string.lower()
-        lowerQuery = query.lower()
-        i = lowerString.find(lowerQuery)
-        while i != -1:
+        # 3. remove redundant parentheses
+        #    goes backwards for step 4
+        redundancies = []
+        i = 0
+        for j in range(len(string) - 1, -1, -1):
+            if (string[j] == "("):
+                i -= 1
+            if ((string[j] == "(" or string[j] == ")") and i > 0):
+                redundancies.append(j)
+            if (string[j] == ")"):
+                i += 1
 
-            # Fix so that this doesn't remove bolding on names
+        for j in redundancies:
+            string = string[:j] + string[j + 1:]
 
-            string = string[:i]+"\033[35m"+string[i:i+len(query)]+"\033[0m"+string[i+len(query):]
-            lowerString =lowerString[:i]+(" " * (len(query)+9))+lowerString[i+len(query):]
-            i = lowerString.find(lowerQuery)
+        # 4. replace parentheses with color escape codes
+        return string.replace("(", "\033[35m").replace(")", "\033[0m")
 
-        return string
+    def __colorSubstring2(string, substring):
+        # 1. get indexes of all occurrences of substring in string
+        #    goes backwards for step 2
+        occurrences = []
+        for i in range(len(string) - len(substring) + 1, -1, -1):
+            if (string[i:i + len(substring)].lower() == substring.lower()):
+                occurrences.append(i)
+
+        # 2. add parentheses around occurrence
+        for occurrence in occurrences:
+            string = string[:occurrence] + "(" + string[occurrence:]
+            string = string[:occurrence + len(substring) + 1] + ")" + string[occurrence + len(substring) + 1:]
+        
+        # 3. remove redundant parentheses
+        #    goes backwards for step 4
+        redundancies = []
+        i = 0
+        for j in range(len(string) - 1, -1, -1):
+            if (string[j] == "("):
+                i -= 1
+            if ((string[j] == "(" or string[j] == ")") and i > 0):
+                redundancies.append(j)
+            if (string[j] == ")"):
+                i += 1
+
+        for j in redundancies:
+            string = string[:j] + string[j + 1:]
+
+        # 4. replace parentheses with color escape codes
+        return string.replace("(", "\033[35m").replace(")", "\033[0m")
+
 
