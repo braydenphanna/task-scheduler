c4d33b08e19ed91591878c00e8c568c5920b9be5
alexphanna <ahanna0608@gmail.com>
2025-10-05 20:29:12 -0400

    Update color substring

--- ui/search.py
+++ ui/search.py
@@ -54,9 +54,11 @@ class Search:
                 occurrences.append(i)
 
         # 2. add parentheses around occurrence
+        letters = list(string)
         for occurrence in occurrences:
-            string = string[:occurrence] + "(" + string[occurrence:]
-            string = string[:occurrence + len(substring) + 1] + ")" + string[occurrence + len(substring) + 1:]
+            letters.insert(occurrence, "(")
+            letters.insert(occurrence + len(substring) + 1, ")")
+        string = ''.join(letters)
         
         # 3. remove redundant parentheses
         #    goes backwards for step 4
@@ -75,36 +77,3 @@ class Search:
 
         # 4. replace parentheses with color escape codes
         return string.replace("(", "\033[35m").replace(")", "\033[0m")
-
-    def __colorSubstring2(string, substring):
-        # 1. get indexes of all occurrences of substring in string
-        #    goes backwards for step 2
-        occurrences = []
-        for i in range(len(string) - len(substring) + 1, -1, -1):
-            if (string[i:i + len(substring)].lower() == substring.lower()):
-                occurrences.append(i)
-
-        # 2. add parentheses around occurrence
-        for occurrence in occurrences:
-            string = string[:occurrence] + "(" + string[occurrence:]
-            string = string[:occurrence + len(substring) + 1] + ")" + string[occurrence + len(substring) + 1:]
-        
-        # 3. remove redundant parentheses
-        #    goes backwards for step 4
-        redundancies = []
-        i = 0
-        for j in range(len(string) - 1, -1, -1):
-            if (string[j] == "("):
-                i -= 1
-            if ((string[j] == "(" or string[j] == ")") and i > 0):
-                redundancies.append(j)
-            if (string[j] == ")"):
-                i += 1
-
-        for j in redundancies:
-            string = string[:j] + string[j + 1:]
-
-        # 4. replace parentheses with color escape codes
-        return string.replace("(", "\033[35m").replace(")", "\033[0m")
-
-
