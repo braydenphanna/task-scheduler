1645cbb705fe4cc39fe867b8f883924b47a14094
Alex <ahanna0608@gmail.com>
2025-10-08 16:36:20 -0400

    Update date selector

--- ui/date_selector.py
+++ ui/date_selector.py
@@ -1,54 +1,52 @@
 from pynput import keyboard
 from ui.util import Util
+import pywinctl
 
 class DateSelector:
     date = []
     i = 0
+    windowTitle = ""
 
     def start():
-        DateSelector.date = list("00/00/00 12:00 PM")
+        DateSelector.windowTitle = pywinctl.getActiveWindowTitle()
+        DateSelector.date = list("00/00/00 00:00 PM")
         i = 0
-        print(DateSelector.date)
-        print("".join(DateSelector.date), end=f"\r{"".join(DateSelector.date[:DateSelector.i])}", flush=True)
+        DateSelector.__print_date()
         with keyboard.Listener(on_press=DateSelector.__on_press) as listener:
             listener.join()
+
+    def __print_date():
+        print("".join(DateSelector.date), end=f"\r{"".join(DateSelector.date[:DateSelector.i])}", flush=True)
     
     def __on_press(key):
-        # if Search.windowTitle == pywinctl.getActiveWindowTitle():
-        #     Util.clear_screen()
-        Util.clear_screen()
+        if DateSelector.windowTitle == pywinctl.getActiveWindowTitle():
+            Util.clear_screen()
 
-        try:
-            if (DateSelector.date[DateSelector.i] != "/"):
-                DateSelector.date[DateSelector.i] = key.char
-                DateSelector.i += 1
-                if (DateSelector.date[DateSelector.i] == "/" or DateSelector.date[DateSelector.i] == " "):
+            try:
+                if (DateSelector.i < 15 and DateSelector.date[DateSelector.i] not in [" ", "/", ":"] and key.char.isdigit()):
+                    DateSelector.date[DateSelector.i] = key.char
                     DateSelector.i += 1
+                    if (DateSelector.date[DateSelector.i] in [" ", "/", ":"]):
+                        DateSelector.i += 1
+                elif (DateSelector.i == 15 and key.char in ["a", "A", "p", "P"]):
+                    DateSelector.date[DateSelector.i] = key.char.upper()
+                    DateSelector.i += 2 # skips past M in AM and PM
+                
+                if (DateSelector.i > len(DateSelector.date)):
+                    return False
 
-        except AttributeError:
-            # esc: exit the search menu
-            if key == keyboard.Key.esc:
-                # Stop listener
-                return False
-            
-            # enter
-            elif key == keyboard.Key.enter:
-                pass
 
-            # backspace
-            elif key == keyboard.Key.backspace:
-                pass
-            
-            # space
-            elif key == keyboard.Key.space:
-                pass
+            except AttributeError:
+                if key == keyboard.Key.esc:
+                    return False
+                elif key == keyboard.Key.enter:
+                    return False
+                elif key == keyboard.Key.backspace:
+                    if (DateSelector.i < 15):
+                        DateSelector.date[DateSelector.i] = "0"
+                    DateSelector.i -= 1
+                    if (DateSelector.date[DateSelector.i] in [" ", "/", ":"]):
+                        DateSelector.i -= 1
+                    pass
 
-            # right arrow
-            elif key == keyboard.Key.right:
-                pass
-
-            # left arrow
-            elif key == keyboard.Key.left and Search.page>1:
-                pass
-
-        print("".join(DateSelector.date), end=f"\r{"".join(DateSelector.date[:DateSelector.i])}", flush=True)
+            DateSelector.__print_date()
